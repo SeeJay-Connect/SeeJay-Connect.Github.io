f67056b77b67d73bf50aa177e46cd429b16f3826
SeeJay-Connect <seejay.connect@gmail.com>
2025-06-21 18:20:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<title>Help Connect</title>
+<link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+<h1>Help Connect</h1>
+<p>This is the landing page.</p>
+<script src="app.js"></script>
+</body>
+</html>
